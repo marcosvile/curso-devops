e341a7a600542b61cc4a129b276dc43f7df4992b
marcosvile <marcosvilelateixeira@gmail.com>
2022-08-23 22:27:53 -0300

    configurando gitignore

--- ansible-lab/vmsup.sh
+++ ansible-lab/vmsup.sh
@@ -1,2 +1,7 @@
 #!/bin/bash
-cd /home/marcos/Documentos/GitHub/curso-devops/ansible-lab/app01
+cd /home/marcos/Documentos/GitHub/curso-devops/ansible-lab/app01 # change to the directory where you have your vagrantfile
+vagrant up  # inicia o ambiente virtual
+cd /home/marcos/Documentos/GitHub/curso-devops/ansible-lab/control-node # change to the directory where you have your vagrantfile
+vagrant up # inicia o ambiente virtual
+cd /home/marcos/Documentos/GitHub/curso-devops/ansible-lab/db01 # change to the directory where you have your vagrantfile
+vagrant up # inicia o ambiente virtual
