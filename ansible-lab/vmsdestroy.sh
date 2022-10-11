8f053565b25e3df412b69e6d6561b6931f816411
marcosvile <marcosvilelateixeira@gmail.com>
2022-10-11 07:57:32 -0300

    novo laboratório

--- ansible-lab/vmsdestroy.sh
+++ ansible-lab/vmsdestroy.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-cd /home/marcos/Documentos/GitHub/curso-devops/ansible-lab/app01 # change to the directory where you have your vagrantfile
+cd ../ansible-lab/app01 # change to the directory where you have your vagrantfile
 vagrant destroy  # apaga o ambiente virtual
-cd /home/marcos/Documentos/GitHub/curso-devops/ansible-lab/control-node # change to the directory where you have your vagrantfile
+cd ..
+cd ../ansible-lab/control-node # change to the directory where you have your vagrantfile
 vagrant destroy  # apaga o ambiente virtual
-cd /home/marcos/Documentos/GitHub/curso-devops/ansible-lab/db01 # change to the directory where you have your vagrantfile
+cd ..
+cd ../ansible-lab/db01 # change to the directory where you have your vagrantfile
 vagrant destroy  # apaga o ambiente virtual
--- ansible-lab/vmsrld.sh
+++ ansible-lab/vmsrld.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
-cd /home/marcos/Documentos/GitHub/curso-devops/ansible-lab/app01 # change to the directory where you have your vagrantfile
-vagrant reload # inicia o ambiente virtual e recarrega os provisions
-cd /home/marcos/Documentos/GitHub/curso-devops/ansible-lab/control-node # change to the directory where you have your vagrantfile
-vagrant reload # inicia o ambiente virtual e recarrega os provisions
-cd /home/marcos/Documentos/GitHub/curso-devops/ansible-lab/db01 # change to the directory where you have your vagrantfile
-vagrant reload # inicia o ambiente virtual e recarrega os provisions
+cd ../ansible-lab/app01 # change to the directory where you have your vagrantfile
+vagrant reload --provision # inicia o ambiente virtual e recarrega os provisions
+cd ..
+cd ../ansible-lab/control-node # change to the directory where you have your vagrantfile
+vagrant reload --provision # inicia o ambiente virtual e recarrega os provisions
+cd ..
+cd ../ansible-lab/db01 # change to the directory where you have your vagrantfile
+vagrant reload --provision # inicia o ambiente virtual e recarrega os provisions
