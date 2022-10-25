934c713a2ae9134d57342f84bf033631acd6f29f
marcosvile <marcosvilelateixeira@gmail.com>
2022-10-25 17:14:49 -0300

    atu

--- ansible-lab/control-node/provision.sh
+++ ansible-lab/control-node/provision.sh
@@ -8,4 +8,5 @@ cat <<EOT >> /etc/hosts
 192.168.56.2 control-node
 192.168.56.3 app01
 192.168.56.4 db01
+192.168.56.5 docker01
 EOT
--- ansible-lab/db01/provision.sh
+++ ansible-lab/db01/provision.sh
@@ -4,6 +4,7 @@ cat <<EOT >> /etc/hosts
 192.168.56.2 control-node
 192.168.56.3 app01
 192.168.56.4 db01
+192.168.56.5 docker01
 EOT
 cat <<EOT >> /home/vagrant/.ssh/authorized_keys
 ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDmaqSMEkXlqcGw55caPYyHHzsk1OWyH9xjwH74TLDaKiCqOJ8lpJu3a7t6Q8Vb/6BapusCU7yZJNhu4LVKszy8SozbImCsCjs747UHfDO/GLNz/pdgDAVVejXOXOg/lxD549geOVgI5w/GMF525/xIJqumznQNkXDP7+cIROA4OXavodKCK06ctTEPAXyLDB9FLtB8pp4y57EF8EskLWAtDAYr62owgeFne54lCMkBBH7e9m6O7fDXeL+f5E22oltFAHRLqu8qBZ0oyxdjeg/v9uZft5WxpDE+AcwaTXI1bcnBzQU9WqpfRnZ5GLfBa1H09im2hHYuRaiNBct6KW7V vagrant@control-node
